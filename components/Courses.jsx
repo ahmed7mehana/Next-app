bd0369dae8da5cd6dafb09912ca5b0241804bf81
ahmed <ahmed7mehana@gmail.com>
2023-04-25 20:51:11 +0200

    my change

--- components/Courses.jsx
+++ components/Courses.jsx
@@ -11,8 +11,6 @@ import { collection, deleteDoc, onSnapshot, query,doc } from 'firebase/firestore
 
 
 function Courses() {
-<<<<<<< HEAD
-
 const { t, i18n } = useTranslation();
  const[data ,setdata]=useState([])
  useEffect(()=>{
@@ -38,11 +36,6 @@ const handle=()=>{
   setshow(!show)
 }
 
-
-=======
-  const { t, i18n } = useTranslation();
->>>>>>> 3fbdc26695c94d029e831c485116f81116713ae9
-
   return (
     <div id='Courses'>
  <h2 className='text-center uppercase font-bold m-5 text-4xl text-white'>{t('Battlefield')}</h2>
@@ -57,10 +50,9 @@ const handle=()=>{
     <div className='flex flex-col justify-center items-center mb-7 mt-7 w-full'>
     <p className="uppercase font-bold  rounded bg-white p-1 px-2 mb-3 w-full text-center text-lg  ">{t('its time to be a superior version of yourself')}</p>
     <div className='flex flex-col md:flex-row justify-center items-center flex-wrap '>
-<<<<<<< HEAD
-=======
 
->>>>>>> 3fbdc26695c94d029e831c485116f81116713ae9
+
+
     </div>
     </div>
     {Data.map((item)=>(
